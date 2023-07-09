5930c92f0ceb8eeff415e634544fb43c5f56e437
YeinerCQ <ycarvajalq@gmail.com>
2023-07-08 23:02:06 -0500

    add turbo_stream behaviour to inventory_movement create action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
 
+  def nested_dom_id(*args)
+    args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
+  end
+
 end
