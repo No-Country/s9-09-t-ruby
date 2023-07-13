8711f244b764ecd4301520c28eee081777ce52ac
YeinerCQ <ycarvajalq@gmail.com>
2023-07-13 11:05:10 -0500

    authenticated user view refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # If an user is authenticated, root is dashboard page
+  authenticated :user do
+    root to: 'pages#supplies', as: :authenticated_root
+  end
 
   # Defines the root path route ("/")
   root "pages#home"
